6f207e885f1b060e974dacbef3d18deaafc7c32f
Taras Shedenko <taras.shedenko@gmail.com>
2023-12-13 17:05:21 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import {
+  authRequest,
+  getAll,
+  getById,
+  createUser,
+  deleteUser,
+  loginUser,
+} from "./controllers";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.json());
+
+app.get("/user/all", authRequest, getAll);
+
+app.get("/user/:id", authRequest, getById);
+
+app.post("/user", authRequest, createUser);
+
+app.delete("/user/:id", authRequest, deleteUser);
+
+app.post("/login", authRequest, loginUser);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) =>
+  res.status(500).send(String(err))
+);
+
+const port = process.env.PORT || "3000";
+
+if (process.env.DB_CONN)
+  mongoose
+    .connect(process.env.DB_CONN!)
+    .then(() => {
+      console.log("Connected to DB");
+      app.listen(port, () =>
+        console.log(`Users servive is running at port ${port}`)
+      );
+    })
+    .catch((err) => {
+      console.log("Error during connecting to DB", err);
+      process.exit(1);
+    });
+else console.log("No connection string specified");
